7c7405b9e5798b2663c7dcf7f348a232954d48b1
Yury Dzhishkariani <dziyura@gmail.com>
2022-10-11 14:30:32 -0700

    cleaner component use

--- src/containers/pricing/Pricing.jsx
+++ src/containers/pricing/Pricing.jsx
@@ -6,22 +6,130 @@ import CTA from '../../components/cta/CTA';
 import './pricing.scss';
 
 function Pricing() {
+  const firstRow = [
+    {
+      headerText: 'Corporation / LLC Formations',
+      subHeader:
+        'We’ll get your company up and running, including incorporating or forming with the proper entity, issuing equity, filing the required state and federal forms, and setting up an equity incentive plan.',
+      header: 'Single-Member LLC',
+      pricing: 500,
+      bp1: 'some bul',
+      bp2: 'another bul',
+      bp3: 'yet another bul',
+    },
+    {
+      header: 'Multi-Member LLC',
+      pricing: 1000,
+      bp1: 'some bul',
+      bp2: 'another bul',
+      bp3: 'yet another bul',
+    },
+    {
+      header: 'Corporation',
+      pricing: 1500,
+      bp1: 'some bul',
+      bp2: 'another bul',
+      bp3: 'yet another bul',
+    },
+  ];
+  const secondRow = [
+    {
+      headerText: 'Financings',
+      subHeader:
+        'We’ll get your company up and running, including incorporatiEvery fundraising round has its own unique terms that require a custom approach. Below is our standard pricing for typical fundraising events. Give us a call to discuss your particular needs and expectations, and we will work with you to determine a custom upfront flat-fee for your financing round.g or forming with the proper entity, issuing equity, filing the required state and federal forms, and setting up an equity incentive plan.',
+      header: 'SAFE Financings',
+      pricing: 1000,
+      bp1: 'some bul',
+      bp2: 'another bul',
+      bp3: 'yet another bul',
+    },
+    {
+      header: 'Convertible Note Financings',
+      pricing: 1500,
+      bp1: 'some bul',
+      bp2: 'another bul',
+      bp3: 'yet another bul',
+    },
+    {
+      header: 'Venture Financings (e.g., Series Seed)',
+      pricing: 15000,
+      bp1: 'some bul',
+      bp2: 'another bul',
+      bp3: 'yet another bul',
+    },
+  ];
+  const thirdRow = [
+    {
+      headerText: 'Financings',
+      subHeader:
+        'We’ll get your company up and running, including incorporatiEvery fundraising round has its own unique terms that require a custom approach. Below is our standard pricing for typical fundraising events. Give us a call to discuss your particular needs and expectations, and we will work with you to determine a custom upfront flat-fee for your financing round.g or forming with the proper entity, issuing equity, filing the required state and federal forms, and setting up an equity incentive plan.',
+      header: 'SAFE Financings',
+      pricing: 1000,
+      bp1: 'some bul',
+      bp2: 'another bul',
+      bp3: 'yet another bul',
+    },
+    {
+      header: 'Convertible Note Financings',
+      pricing: 1500,
+      bp1: 'some bul',
+      bp2: 'another bul',
+      bp3: 'yet another bul',
+    },
+    {
+      header: 'Venture Financings (e.g., Series Seed)',
+      pricing: 15000,
+      bp1: 'some bul',
+      bp2: 'another bul',
+      bp3: 'yet another bul',
+    },
+  ];
   return (
     <div>
       <div />
       <Navbar />
-      <h1 className="headerText">CORPORATION/LLC FORMATIONS</h1>
-      <h2 className="sub-header-text">Our Corporation/LLC Formation packages all include a meeting to discuss the formation process including the pros and cons of entity types, equityholder agreements, voting rights, economic rights</h2>
-      <PricingTable />
-      <h1 className="headerText">FINANCINGS</h1>
-      <h2 className="sub-header-text">Our Corporation/LLC Formation packages all include a meeting to discuss the formation process including the pros and cons of entity types, equityholder agreements, voting rights, economic rights</h2>
-      <PricingTable />
-      <h1 className="headerText">CUSTOM CONTRACT TEMPLATES</h1>
-      <h2 className="sub-header-text">Our Corporation/LLC Formation packages all include a meeting to discuss the formation process including the pros and cons of entity types, equityholder agreements, voting rights, economic rights</h2>
-      <PricingTable />
-      <h1 className="headerText">POLICIES AND TERMS OF SERVICE</h1>
-      <h2 className="sub-header-text">Our Corporation/LLC Formation packages all include a meeting to discuss the formation process including the pros and cons of entity types, equityholder agreements, voting rights, economic rights</h2>
-      <PricingTable />
+      <div className="container group">
+        {firstRow.map((item, index) => (
+          <PricingTable
+            headerText={item.headerText}
+            subHeader={item.subHeader}
+            header={item.header}
+            pricing={item.pricing}
+            bp1={item.bp1}
+            bp2={item.bp2}
+            bp3={item.bp3}
+            index={index}
+          />
+        ))}
+      </div>
+      <div className="container group">
+        {secondRow.map((item, index) => (
+          <PricingTable
+            headerText={item.headerText}
+            subHeader={item.subHeader}
+            header={item.header}
+            pricing={item.pricing}
+            bp1={item.bp1}
+            bp2={item.bp2}
+            bp3={item.bp3}
+            index={index}
+          />
+        ))}
+      </div>
+      <div className="container group">
+        {thirdRow.map((item, index) => (
+          <PricingTable
+            headerText={item.headerText}
+            subHeader={item.subHeader}
+            header={item.header}
+            pricing={item.pricing}
+            bp1={item.bp1}
+            bp2={item.bp2}
+            bp3={item.bp3}
+            index={index}
+          />
+        ))}
+      </div>
       <div className="spliter" />
       <CTA />
       <Footer />
--- src/containers/pricing/components/PricingTable.jsx
+++ src/containers/pricing/components/PricingTable.jsx
@@ -1,79 +1,23 @@
 import React from 'react';
 
-function pricingTable() {
+function pricingTable({ headerText, subHeader, header, pricing, bp1, bp2, bp3 }) {
   return (
-    <div className="container group">
+    // <div className="container group">
+    <div>
+      <h1 className="headerText">{headerText}</h1>
+      <h2 className="sub-header-text">
+        {subHeader}
+      </h2>
       <div className="grid-1-5">
-        <h2>Single-Owner LLC Formation</h2>
+        <h2>{header}</h2>
         <h3>
-          <span className="uppercase">$500</span>
+          <span className="uppercase">${pricing}</span>
         </h3>
         <ul>
-          <li>Limited Email Support</li>
-          <li>Unlimited Data Transfer</li>
-          <li>10GB Local Storage</li>
+          <li>{bp1}</li>
+          <li>{bp2}</li>
+          <li>{bp3}</li>
         </ul>
-        <a href="button" className="button">
-          Sign Up
-        </a>
-      </div>
-      <div className="grid-1-5">
-        <h2>Multi-Owner LLC Formation</h2>
-        <h3>
-          <sup>$</sup>1,000
-        </h3>
-        <ul>
-          <li>Limited Email Support</li>
-          <li>Unlimited Data Transfer</li>
-          <li>20GB Local Storage</li>
-        </ul>
-        <a href="button" className="button">
-          Sign Up
-        </a>
-      </div>
-      <div className="grid-1-5">
-        <h2>Corporation Formation</h2>
-        <h3>
-          {/* <sup>$</sup>1500 */}
-        </h3>
-        <ul>
-          <li>Email Support</li>
-          <li>Unlimited Data Transfer</li>
-          <li>30GB Local Storage</li>
-        </ul>
-        <a href="button" className="button">
-          $1500
-        </a>
-      </div>
-      <div className="grid-1-5">
-        <h2>Non-Profit or B-Corporation Formation</h2>
-        <h3>
-          <sup>$</sup>1500
-        </h3>
-        <ul>
-          <li>Email Support</li>
-          <li>Phone Support</li>
-          <li>Unlimited Data Transfer</li>
-        </ul>
-        {/* <a href="button" className="button">
-          Sign Up
-        </a> */}
-      </div>
-      <div className="grid-1-5">
-        <h2>Custom Contract Templates</h2>
-        <ul>
-          <li>Advisor Agreement</li>
-          <li>Consulting Agreement</li>
-          <li>Brand Ambassador Agreement</li>
-          <li>Influencer Sponsorship Agreement</li>
-          <li>Content Collaboration agreement</li>
-          <li>Social Media Appearance Agreement</li>
-          <li>Non-Disclosure Agreement</li>
-          <li>Employee Offer Letter</li>
-        </ul>
-        <a href="button" className="button">
-          Sign Up
-        </a>
       </div>
     </div>
   );
