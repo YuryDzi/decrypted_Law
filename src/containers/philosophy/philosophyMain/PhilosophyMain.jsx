8955ee8110d154c4b831bd7a1fa7631e0ee06520
Yury Dzhishkariani <dziyura@gmail.com>
2022-10-11 19:12:05 -0700

    added text to philosophy + formated it a bit

--- src/containers/philosophy/philosophyMain/PhilosophyMain.jsx
+++ src/containers/philosophy/philosophyMain/PhilosophyMain.jsx
@@ -7,29 +7,39 @@ function PhilosophyMain() {
       <div className="banner d-flex align-items-center">
         <div className="banner-left">
           <h1>
-            Transparent <br /> Modern <br /> Property
+            This is Decrypted Legal &amp; Consulting (Decrypted for short).
           </h1>
           <div className="inner-desc">
             <p>
-              we are here to make your journey to a new home, simple and
-              memorable. So we decided to bring you a platform which allows you
-              to browse through millions of property listings across 900+ cities
-              in India. Now buying and renting comes with the freedom of ease
-              and comfort.{' '}
+              <br /> We
+              founded Decrypted as a new type of legal services provider run by
+              former “Big Law” attorneys who sensed there must be a better way
+              to serve entrepreneurs.Our Approach Most entrepreneurs complain
+              that contacting an attorney feels like an emergency room visit.
+              That the services are ambiguous and confusing. That they struggle
+              with knowing the risks or the benefits of preventative legal care
+              until it is too late. <br /> <br /> While many attorneys typically over promise
+              and under deliver, they still manage to drop off surprisingly high
+              bills after each consultation or project. We designed Decrypted
+              differently. Decrypted runs more like a business and less like a
+              law firm. We are innovative and proactive. Our pricing is
+              transparent. We provide subscription plans for those seeking
+              ongoing legal guidance, without ongoing uncertainty about monthly
+              cost. We use upfront fixed-fee pricing for your specific projects.
+              We readily provide “bundles&quot; for many popular and standard
+              services. We also use the same tools as you -- Zoom, Dropbox,
+              Asana, Slack, Calendly, Google Workspace, DocuSign, Stripe, and
+              many more -- to make our communication and partnership as seamless
+              and effective as possible. <br /> <br /> Although many attorneys focus on
+              reasons why you can’t do something, we focus on finding innovative
+              solutions and “yes if” answers to facilitate your ideas and
+              ambitions. We don’t bury you in legalese. We tell you in plain
+              English which risks are worth taking and which aren’t. We’re here
+              to be your confidante and ally. To be someone who can help you
+              navigate the legal landscape so that when you’re lost or unsure,
+              we can guide you down the right path. Not someone you reluctantly
+              call when you are out of options.{' '}
             </p>
-            <form>
-              <div className="form-group d-flex flex-wrap">
-                <input
-                  type="text"
-                  className="form-control"
-                  id="exampleInputtext1"
-                  placeholder="Enter zipcode to search properties here"
-                />
-                <button type="submit" className="black-btn">
-                  Search
-                </button>
-              </div>
-            </form>
           </div>
           <div className="social-icons d-flex align-items-center">
             <ul className="d-flex">
@@ -49,7 +59,6 @@ function PhilosophyMain() {
                 </a>
               </li>
             </ul>
-            <span>Follow us</span>
           </div>
         </div>
         <div className="banner-right d-flex">
@@ -67,7 +76,7 @@ function PhilosophyMain() {
             <div className="banner-right-inner">
               <h2>42k+</h2>
               <span>
-                satisfied <br /> family
+                satisfied <br /> clients
               </span>
               <img
                 src="https://yudiz.com/codepen/real-estate/heart-icon.svg"
@@ -100,7 +109,7 @@ function PhilosophyMain() {
             <div className="banner-right-inner">
               <h2>30k+</h2>
               <span>
-                Available <br /> Unit for Sale
+                Won <br /> Cases
               </span>
               <img
                 src="https://yudiz.com/codepen/real-estate/unit-icon.svg"
