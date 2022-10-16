f216c344d1883889e8b368edcb5fc50ef27bf8f3
Yury Dzhishkariani <dziyura@gmail.com>
2022-10-16 11:07:40 -0700

    added contact buttons (choose the right collor)

--- src/components/contactInput/ContactInput.jsx
+++ src/components/contactInput/ContactInput.jsx
@@ -3,17 +3,28 @@ import './contactInput.scss';
 
 function ContactInput() {
   return (
-    <div className="container-input">
-      {/* <div className="demo-flex-spacer" />   */}
-      <div className="webflow-style-input">
-        <input className="" type="name" placeholder="First & Last Name" />
-        <input className="" type="email" placeholder="What's your email?" />
-        <button type="button"> Send </button>
-        {/* <button type="submit">
-          <i className="icon ion-android-arrow-forward" />
-        </button> */}
-      </div>
-      {/* <div className="demo-flex-spacer" /> */}
+    <div className="button__class">
+      <a href="button">
+        <span />
+        <span />
+        <span />
+        <span />
+        Call Us
+      </a>
+      <a href="button">
+        <span />
+        <span />
+        <span />
+        <span />
+        Call Us
+      </a>
+      <a href="button">
+        <span />
+        <span />
+        <span />
+        <span />
+        Call Us
+      </a>
     </div>
   );
 }
