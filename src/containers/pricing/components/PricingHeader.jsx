b069f2f38720b1a0ef82aac3503bade18a780d9e
Yury Dzhishkariani <dziyura@gmail.com>
2022-10-16 11:00:03 -0700

    text edditions

--- src/containers/pricing/components/PricingHeader.jsx
+++ src/containers/pricing/components/PricingHeader.jsx
@@ -3,17 +3,17 @@ import React from 'react';
 function PricingHeader() {
   return (
     <div>
-      <h1 className="headerText">Pricing</h1>
+      {/* <h1 className="headerText">Pricing</h1> */}
       <h3 className="sub-header-text">
         We don&#39;t like hourly billing and we&#39;re guessing you don&#39;t
         either. You have enough to worry about as an entrepreneur, so why add
-        the worry of unpredictable legal bills? We only use upfront fixed fees
+        the worry of unpredictable legal bills? <br /> We only use upfront fixed fees
         and created a series of “bundles” for sets of popular and essential
-        legal matters we commonly perform. If you’re interested in having more
+        legal matters we commonly perform. <br />If you’re interested in having more
         of a constant confidante than just a lawyer, we also offer on-demand
         general counsel work with our subscription pricing, so you don’t have to
         fear racking up expensive billable hours each month when asking
-        questions and seeking guidance. Outlined below are some of our bundles
+        questions and seeking guidance. <br />Outlined below are some of our bundles
         with approximate prices. However, if you don’t see what you need (or
         aren’t even sure what you need), just contact us and we will work with
         you to provide guidance and a fair fixed fee.
