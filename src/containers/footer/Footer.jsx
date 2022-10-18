fb162c92043c6eb1a56efc3273350af8d6e002c1
Yury Dzhishkariani <dziyura@gmail.com>
2022-10-17 18:53:28 -0700

    edits to footer links and navbar css

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import gpt3Logo from '../../logo.svg';
 import './footer.css';
 import FaqTable from '../FAQ/Faq';
 import ContactInput from '../../components/contactInput/ContactInput';
@@ -15,32 +14,22 @@ const Footer = () => (
     <FaqTable />
     <div className="gpt3__footer-links">
       <div className="gpt3__footer-links_logo">
-        <img src={gpt3Logo} alt="gpt3_logo" />
-        <p>355 S. Grand Ave., Ste. 2450, Los Angeles, CA 90071<br /> All Rights Reserved</p>
-      </div>
-      <div className="gpt3__footer-links_div">
-        <h4>Links</h4>
-        <p>Overons</p>
-        <p>Social Media</p>
-        <p>Counters</p>
-        <p>Contact</p>
+        <p>Decrypted Law <br /><br /> All Rights Reserved</p>
       </div>
       <div className="gpt3__footer-links_div">
         <h4>Company</h4>
         <p>Terms & Conditions </p>
         <p>Privacy Policy</p>
-        <p>Contact</p>
       </div>
       <div className="gpt3__footer-links_div">
         <h4>Get in touch</h4>
-        <p>355 S. Grand Ave., Ste. 2450, Los Angeles, CA 90071</p>
-        <p>085-132567</p>
+        <p>424 276-10327</p>
         <p>hello@decryptedlaw.com</p>
       </div>
     </div>
 
     <div className="gpt3__footer-copyright">
-      <p>@2021 GPT-3. All rights reserved.</p>
+      <p>@2022 Decrypted Legal & Consulting.  All rights reserved.</p>
     </div>
   </div>
 );
