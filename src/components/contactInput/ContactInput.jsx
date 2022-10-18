1eb5e88ac9ccebb36a60a2effb9dcf610b4d9e53
Yury Dzhishkariani <dziyura@gmail.com>
2022-10-17 18:19:18 -0700

    added call us button with number to contact

--- src/components/contactInput/ContactInput.jsx
+++ src/components/contactInput/ContactInput.jsx
@@ -4,21 +4,7 @@ import './contactInput.scss';
 function ContactInput() {
   return (
     <div className="button__class">
-      <a href="button">
-        <span />
-        <span />
-        <span />
-        <span />
-        Call Us
-      </a>
-      <a href="button">
-        <span />
-        <span />
-        <span />
-        <span />
-        Call Us
-      </a>
-      <a href="button">
+      <a href="tel:+14242761032">
         <span />
         <span />
         <span />
