ca40f6704c48de090528b2bd1e5a5b269de89eae
Yury Dzhishkariani <dziyura@gmail.com>
2022-10-24 16:33:24 -0700

    added contact us input form based on customer req

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -1,16 +1,54 @@
-import React from 'react';
-import './cta.css';
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
+import './cta.scss';
 /* eslint-disable */
-const CTA = () => (
-  <div className="gpt3__cta">
-    <div className="gpt3__cta-content">
-      <p>Contact Us</p>
-      <h3>Set up a free 30-minute consultation today</h3>
+function CTA() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "replace with service id",
+        "replace with template id",
+        form.current,
+        "replace with user id"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          console.log("message sent");
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+  return (
+    <div className="gpt3__cta">
+      <div className="gpt3__cta-content">
+        <form className="form-gpt3" ref={form} onSubmit={sendEmail}>
+          <label>First & Last Name</label>
+          <input type="text" name="user_name" />
+          <label>Email</label>
+          <input type="email" name="user_email" />
+          <label>Message</label>
+          <textarea className="message-gpt3" name="message" />
+          <div className="gpt3__cta-btn">
+            <button type="button" ref={form} onClick={sendEmail}>
+              Send
+            </button>
+          </div>
+          {/* <input type="submit" value="Send" /> */}
+        </form>
+      </div>
+      {/* <div className="gpt3__cta-btn">
+      <button type="button" ref={form} onClick={sendEmail}>Send</button>
+    </div> */}
     </div>
-    <div className="gpt3__cta-btn">
-      <button type="button" onClick={() => window.location.href = 'mailto:hello@decryptedlaw.com'}>Email us</button>
-    </div>
-  </div>
-);
+  );
+}
 
 export default CTA;
